1d174bef9f01a0f36894dff5014f9862da160fb2
Bodhi <bodhilooij1998@gmail.com>
2022-04-14 15:50:46 +0200

    Added resources to match the new endpoints of the HP api.

---
+++ src/Resources/FunctionFeature.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace WeDesignIt\HotelprofessionalsApiClient\Resources;
+
+use WeDesignIt\HotelprofessionalsApiClient\Traits\ViewResource;
+
+class FunctionFeature extends Resource
+{
+    use ViewResource;
+}
